c591a66a16e331d8b3768a63432ea5d126f3ed4d
amit <[amirana14325@gmail.com]>
2021-03-12 13:31:59 +0530

    [Amit Rana]Added::add first contact

--- Address book/Address book/Program.cs
+++ Address book/Address book/Program.cs
@@ -2,11 +2,37 @@
 
 namespace Address_book
 {
+
+    public static
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
             Console.WriteLine("Welcome to Addresh book using console Apllicaation by Amit Rana");
+            setContactDetails();
         }
+        public static void setContactDetails()
+        {
+            Contact obj = new Contact();
+                Console.WriteLine("Enter the First Name");
+                obj.FirstName = Console.ReadLine();
+                Console.WriteLine("Enter the Last Name");
+                obj.LastName = Console.ReadLine();
+                Console.WriteLine("Enter the Address");
+                obj.Address = Console.ReadLine();
+                Console.WriteLine("Enter the City Name");
+                obj.City = Console.ReadLine();
+                Console.WriteLine("Enter the State Name");
+                obj.State = Console.ReadLine();
+                Console.WriteLine("Enter the zip code");
+                obj.Zip = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the Phone Number");
+                obj.PhoneNumber = long.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the email address");
+                obj.Email = Console.ReadLine();
+            
+        }
+        
     }
+
 }
